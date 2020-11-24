95f6be1c38964e0cc5579ec873d580d9697fda76
laper32 <laper32@outlook.com>
2020-11-24 10:59:46 +0000

    sdktools_stringtable module import

--- include/sm/sdktools.h
+++ include/sm/sdktools.h
@@ -80,7 +80,7 @@ namespace sm {
         }
 
         inline bool LockStringTables(bool lock) {
-            return engine->LockNetworkStringTables(lock) ? 1 : 0;
+            return engine->LockNetworkStringTables(lock) ? true : false;
         }
 
         inline int FindStringTable(std::string _Table) 
@@ -105,6 +105,87 @@ namespace sm {
             pTable->AddString(true, length, userdata.c_str());
 #endif
             return true;
+        }
+
+        inline int GetNumStringTables() { return netstringtables->GetNumTables(); }
+
+        inline int GetStringTableNumStrings(TABLEID idx) {
+            INetworkStringTable* pTable = netstringtables->GetTable(idx);
+            if (!pTable) smutils->LogError(myself, "Invalid string table index: %d", idx);
+            return pTable->GetNumStrings();
+        }
+
+        inline int GetStringTableMaxStrings(TABLEID idx) {
+            INetworkStringTable* pTable = netstringtables->GetTable(idx);
+            if (!pTable) smutils->LogError(myself, "Invalid string table index: %d", idx);
+            return pTable->GetMaxStrings();
+        }
+
+        inline void GetStringTableName(TABLEID idx) {
+            INetworkStringTable* pTable = netstringtables->GetTable(idx);
+            if (!pTable) smutils->LogError(myself, "Invalid string table index: %d", idx);
+            // HELP: Gets a suitable return.
+            /*size_t numBytes;
+
+            if (!pTable)
+            {
+                return pContext->ThrowNativeError("Invalid string table index %d", idx);
+            }
+
+            pContext->StringToLocalUTF8(params[2], params[3], pTable->GetTableName(), &numBytes);
+
+            return numBytes;*/
+            //return (std::string() + pTable->GetTableName());
+        }
+
+        inline int FindStringIndex(TABLEID idx, std::string _Str) {
+            INetworkStringTable* pTable = netstringtables->GetTable(idx);
+            if (!pTable) smutils->LogError(myself, "Invalid string table index: %d", idx);
+            int strIndex = pTable->FindStringIndex(_Str.c_str());
+            return (strIndex == INVALID_STRING_INDEX) ? -1 : strIndex;
+        }
+
+        inline int ReadStringTable(TABLEID idx, int stringIdx)
+        {
+            INetworkStringTable* pTable = netstringtables->GetTable(idx);
+            if (!pTable) smutils->LogError(myself, "Invalid string table index: %d", idx);
+            std::string value = (std::string() + pTable->GetString(stringIdx));
+            if (!value.size()) smutils->LogError(myself, "Invalid string index specified for table (index: %d) (table: \"%s\")", stringIdx, pTable->GetTableName());
+            // HELP: NumBytes
+            /*
+            pContext->StringToLocalUTF8(params[3], params[4], value, &numBytes);
+
+	        return numBytes;
+            */
+        }
+        //native int GetStringTableDataLength(int tableidx, int stringidx);
+        inline int GetStringTableDataLength(TABLEID idx, int stringIdx)
+        {
+            INetworkStringTable* pTable = netstringtables->GetTable(idx);
+            if (!pTable) smutils->LogError(myself, "Invalid string table index: %d", idx);
+            if (stringIdx < 0 || stringIdx >= pTable->GetNumStrings())
+                smutils->LogError(myself, "Invalid string index specified for table (index %d) (table \"%s\")", stringIdx, pTable->GetTableName());
+            int datalen;
+            const void* userdata = pTable->GetStringUserData(stringIdx, &datalen);
+            return (!userdata) ? 0 : datalen;
+        }
+
+        inline void GetStringTableData(TABLEID idx, int stringIdx) {
+            INetworkStringTable* pTable = netstringtables->GetTable(idx);
+            if (!pTable) smutils->LogError(myself, "Invalid string table index: %d", idx);
+            if (stringIdx < 0 || stringIdx >= pTable->GetNumStrings())
+                smutils->LogError(myself, "Invalid string index specified for table (index %d) (table \"%s\")", stringIdx, pTable->GetTableName());
+            int datalen = 0;
+            std::string userdata = (std::string() + reinterpret_cast<const char*>(pTable->GetStringUserData(stringIdx, &datalen)));
+            int numBytes = std::min(numBytes, datalen);
+            // HELP: remaining
+            //if (userdata) std::memcpy(userdata.c_str(), userdata, numBytes);
+        }
+
+        // HELP ME
+        inline void SetStringTableData(TABLEID idx) {
+            INetworkStringTable* pTable = netstringtables->GetTable(idx);
+            if (!pTable) smutils->LogError(myself, "Invalid string table index: %d", idx);
         }
 
         inline void AddFileToDownloadsTable(std::string _FileName) {
