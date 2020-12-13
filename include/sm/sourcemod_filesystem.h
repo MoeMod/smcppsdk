55e7a3c551515fab15aa816511094481bd9a5f23
laper32 <laper32@outlook.com>
2020-12-13 02:18:25 +0000

    fs temp

--- include/sm/sourcemod_filesystem.h
+++ include/sm/sourcemod_filesystem.h
@@ -2,6 +2,8 @@
 
 #include "filesystem.h"
 #include <filesystem>
+
+// perhaps using std::fs will be easier..
 namespace sm
 {
 	inline namespace sourcemod
@@ -32,10 +34,10 @@ namespace sm
 				return std::filesystem::is_directory(realpath);
 			}
 			// Register functions.
-			class File
-			{
-
-			};
+			//native File OpenFile(const char[] file, const char[] mode, bool use_valve_fs = false, const char[] valve_path_id = "GAME");
+			//native bool DeleteFile(const char[] path, bool use_valve_fs=false, const char[] valve_path_id="DEFAULT_WRITE_PATH");
+			//native bool ReadFileLine(Handle hndl, char[] buffer, int maxlength);
+			//native int ReadFile(Handle hndl, any[] items, int num_items, int size);
 		}
 	}
 }
