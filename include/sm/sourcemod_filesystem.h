269ada67c1f94e639b10f0d574ca3c36c3d856d3
laper32 <laper32@outlook.com>
2020-12-17 01:04:10 +0000

    filesystem OK

--- include/sm/sourcemod_filesystem.h
+++ include/sm/sourcemod_filesystem.h
@@ -11,6 +11,105 @@ namespace sm
 		
 		inline namespace filesystem
 		{
+			class SystemFile
+			{
+			public:
+				SystemFile(FILE* fp)
+					: fp_(fp)
+				{}
+				~SystemFile() {
+					Close();
+				}
+
+				static SystemFile* Open(const char* path, const char* mode) {
+					FILE* fp = fopen(path, mode);
+					if (!fp)
+						return nullptr;
+					return new SystemFile(fp);
+				}
+				static bool Delete(const char* path) {
+					return _unlink(path) == 0;
+				}
+				size_t Read(void* pOut, int size) {
+					return fread(pOut, 1, size, fp_);
+				}
+				char* ReadLine(char* pOut, int size) {
+					return fgets(pOut, size, fp_);
+				}
+				size_t Write(const void* pData, int size) {
+					return fwrite(pData, 1, size, fp_);
+				}
+				bool Seek(int pos, int seek_type) {
+					return fseek(fp_, pos, seek_type) == 0;
+				}
+				int Tell() {
+					return ftell(fp_);
+				}
+				bool HasError() {
+					return ferror(fp_) != 0;
+				}
+				bool Flush() {
+					return fflush(fp_) == 0;
+				}
+				bool EndOfFile() {
+					return feof(fp_) != 0;
+				}
+				void Close() {
+					if (!fp_)
+						return;
+					fclose(fp_);
+					fp_ = nullptr;
+				}
+				virtual SystemFile* AsSystemFile() {
+					return this;
+				}
+				FILE* fp() const {
+					return fp_;
+				}
+			private:
+				FILE* fp_;
+				//std::string _filename;
+
+			};
+
+			inline int32_t ReadFileString(SystemFile* file, char* buffer, int size, int read_count = -1)
+			{
+				int32_t num_read = 0;
+				if (read_count != -1)
+				{
+					if (std::size_t(read_count) > std::size_t(size))
+					{
+						smutils->LogError(myself, "read_count (%u) is greater than buffer size (%u)", read_count, size);
+						return -1;
+					}
+					num_read = (int32_t)file->Read(buffer, read_count);
+					if (num_read != read_count && file->HasError()) return -1;
+
+					return num_read;
+				}
+
+				char val;
+				while (true)
+				{
+					if (!size || num_read >= size - 1) break;
+					if (file->Read(&val, sizeof(val)) != 1)
+					{
+						if (file->HasError()) return -1;
+
+						break;
+					}
+
+					if (val == '\0') break;
+					if (size > 0 && num_read < size - 1)
+					{
+						buffer[num_read++] = val;
+					}
+				}
+
+				if (size > 0) buffer[num_read] = '\0';
+				return num_read;
+			}
+
 			extern IFileSystem* valvefs;
 			inline bool FileExists(const char* path, bool use_valve_fs = false, const char* valve_path_id = "GAME")
 			{
@@ -36,6 +135,20 @@ namespace sm
 			// Register functions.
 			//native File OpenFile(const char[] file, const char[] mode, bool use_valve_fs = false, const char[] valve_path_id = "GAME");
 			//native bool DeleteFile(const char[] path, bool use_valve_fs=false, const char[] valve_path_id="DEFAULT_WRITE_PATH");
+			#undef DeleteFile
+			inline bool DeleteFile(const char* path, bool use_valve_fs = false, const char* valve_path_id = "DEFAULT_WRITE_PATH")
+			{
+				if (use_valve_fs)
+				{
+					valvefs->RemoveFile(path, valve_path_id);
+					return true;
+				}
+
+				char buffer[256];
+				g_pSM->BuildPath(Path_Game, buffer, sizeof(buffer), "%s", path);
+				std::filesystem::remove(buffer);
+				return true;
+			}
 			//native bool ReadFileLine(Handle hndl, char[] buffer, int maxlength);
 			//native int ReadFile(Handle hndl, any[] items, int num_items, int size);
 		}
